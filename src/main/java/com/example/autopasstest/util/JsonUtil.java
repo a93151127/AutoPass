603bd5d6c2bbc13db63c18e9609365c65315eab1
a93151127 <a93151127@gmail.com>
2023-12-28 11:47:22 +0800

    完成 JsonUtil

---
+++ src/main/java/com/example/autopasstest/util/JsonUtil.java
@@ -0,0 +1,24 @@
+package com.example.autopasstest.util;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+public class JsonUtil {
+    public static <T> T jsonToObject(String jsonString, Class<T> obj) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);
+        mapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        return mapper.readValue(jsonString, obj);
+    }
+
+    public static String objectToJson(Object obj) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        return mapper.writeValueAsString(obj);
+    }
+}
